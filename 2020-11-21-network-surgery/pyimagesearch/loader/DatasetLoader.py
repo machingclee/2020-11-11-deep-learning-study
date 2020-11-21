9f90f32699ece53fc3820a4079c39a4133a61570
Ching-Cheong Lee <machingclee@gmail.com>
2020-11-22 01:58:33 +0800

    update

--- 2020-11-21-network-surgery/pyimagesearch/loader/DatasetLoader.py
+++ 2020-11-21-network-surgery/pyimagesearch/loader/DatasetLoader.py
@@ -5,23 +5,43 @@ import os
 
 class DatasetLoader:
     def __init__(self, preprocessors=None):
-        self.preprocessors = [] if preprocessors == None else preprocessors
+        # store the image preprocessor
+        self.preprocessors = preprocessors
+
+        # if the preprocessors are None, initialize them as an
+        # empty list
+        if self.preprocessors is None:
+            self.preprocessors = []
 
     def load(self, imagePaths, verbose=-1):
-        images = []
+        # initialize the list of features and labels
+        data = []
         labels = []
+
+        # loop over the input images
         for (i, imagePath) in enumerate(imagePaths):
+            # load the image and extract the class label assuming
+            # that our path has the following format:
+            # /path/to/dataset/{class}/{image}.jpg
             image = cv2.imread(imagePath)
             label = imagePath.split(os.path.sep)[-2]
 
-            if self.preprocessors != None:
-                for preprocessor in self.preprocessors:
-                    image = preprocessor.preprocess(image)
+            # check to see if our preprocessors are not None
+            if self.preprocessors is not None:
+                # loop over the preprocessors and apply each to
+                # the image
+                for p in self.preprocessors:
+                    image = p.preprocess(image)
 
-            images.append(image)
+            # treat our processed image as a "feature vector"
+            # by updating the data list followed by the labels
+            data.append(image)
             labels.append(label)
 
-            if verbose > 0 and i > 0 and (i+1) % verbose == 0:
-                print("[INFO] processed {}/{}".format(i+1, len(imagePaths)))
+            # show an update every `verbose` images
+            if verbose > 0 and i > 0 and (i + 1) % verbose == 0:
+                print("[INFO] processed {}/{}".format(i + 1,
+                                                      len(imagePaths)))
 
-        return (np.array(images), np.array(labels))
+        # return a tuple of the data and labels
+        return (np.array(data), np.array(labels))
--- 2020-11-21-network-surgery/pyimagesearch/preprocessing/ImageToArrayPreprocessor.py
+++ 2020-11-21-network-surgery/pyimagesearch/preprocessing/ImageToArrayPreprocessor.py
@@ -1,7 +1,5 @@
 from tensorflow.keras.preprocessing.image import img_to_array
 
-# this class mainly deals with PIL Image instance.
-
 
 class ImageToArrayPreprocessor:
     def __init__(self, dataFormat=None):
@@ -9,4 +7,6 @@ class ImageToArrayPreprocessor:
         self.dataFormat = dataFormat
 
     def preprocess(self, image):
+        # apply the Keras utility function that correctly rearranges
+        # the dimensions of the image
         return img_to_array(image, data_format=self.dataFormat)
