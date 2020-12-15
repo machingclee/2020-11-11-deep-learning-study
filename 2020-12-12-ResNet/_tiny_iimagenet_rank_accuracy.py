1a7f5f3f79727566d6e72e2860c31621fa0ce6be
Ching C Lee <machingclee@gmail.com>
2020-12-16 01:55:02 +0800

    update

--- 2020-12-12-ResNet/_tiny_iimagenet_rank_accuracy.py
+++ 2020-12-12-ResNet/_tiny_iimagenet_rank_accuracy.py
@@ -1,4 +1,5 @@
-from config import tiny_imagenet_config as config
+import os
+from config import tinyimagenet_config as config
 from pyimagesearch.preprocessing import ImageToArrayPreprocessor
 from pyimagesearch.preprocessing import ResizePreprocessor
 from pyimagesearch.preprocessing import MeanSubtractionPreprocessor
@@ -7,10 +8,10 @@ from pyimagesearch.io import HDF5DatasetGenerator
 from tensorflow.keras.models import load_model
 import json
 
-FINAL_MODEL_PATH = None
+FINAL_MODEL_PATH = os.path.sep.join(["output", "checkpoints", "ResNet-TinyImagenet-200-4-epoch-75.hdf5"])
 
 
-means = json.loads(open(config.DATASET_MEAN).read())
+means = json.loads(open(config.MEAN_JSON_PATH).read())
 
 resize_pp = ResizePreprocessor(64, 64)
 mean_subtraction_pp = MeanSubtractionPreprocessor(means["R"], means["G"], means["B"])
@@ -18,7 +19,7 @@ img_to_arr_pp = ImageToArrayPreprocessor()
 
 test_generator = HDF5DatasetGenerator(config.TEST_HDF5_PATH, 64,
                                       preprocessors=[resize_pp, mean_subtraction_pp, img_to_arr_pp],
-                                      n_classes=config.config.NUM_CLASSES)
+                                      n_classes=200)
 
 if FINAL_MODEL_PATH is not None:
     model = load_model(FINAL_MODEL_PATH)
@@ -26,7 +27,7 @@ if FINAL_MODEL_PATH is not None:
                           steps=test_generator.numOfImages//64,
                           max_queue_size=10
                           )
-    (rank1, rank5) = rank5_accuracy(preds=preds, label=test_generator.db["labels"])
+    (rank1, rank5) = rank5_accuracy(preds=preds, labels=test_generator.db["labels"])
     print("[INFO] rank-1: {:.2f}%".format(rank1*100))
     print("[INFO] rank-5: {:.2f}%".format(rank5*100))
 
