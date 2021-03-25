942e4ea0ffaab8477d1fb52c6da71b305def4359
Ching C Lee <machingclee@gmail.com>
2021-03-26 04:51:13 +0800

    update

--- 2021-03-21-poetry-generation/wseq2seq.py
+++ 2021-03-21-poetry-generation/wseq2seq.py
@@ -1,11 +1,8 @@
 import os
-import sys
-import string
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, Embedding, Input, LSTM
 from tensorflow.keras.preprocessing.text import Tokenizer
@@ -14,194 +11,311 @@ from tensorflow.keras.optimizers import Adam,  SGD
 from tensorflow.python.distribute.distribute_coordinator import _get_num_workers
 from tensorflow.python.keras.backend import dropout
 
-BATCH_SIZE = 64
-EPOCHS = 100
-LATENT_DIM = 256
-NUM_SAMPLES = 10000
-MAX_SEQUENCE_LENGTH = 100
-MAX_NUM_WORDS = 20000
-EMBEDDING_DIM = 100
 
-input_texts = []
-target_texts = []
-target_texts_inputs = []
+class WSeq2Seq:
+    BATCH_SIZE = 64
+    EPOCHS = 100
+    LATENT_DIM = 256
+    NUM_SAMPLES = 10000
+    MAX_SEQUENCE_LENGTH = 100
+    MAX_VOCAB_SIZE = 20000
+    EMBEDDING_DIM = 100
+
+    encoder_texts_input = []
+    decoder_texts_output = []
+    decoder_texts_input = []
+
+    encoder_input_seq = []
+    decoder_output_seq = []
+    decoder_input_seq = []
+
+    encoder_input_tokenizer: Tokenizer = None
+    decoder_input_tokenizer: Tokenizer = None
+
+    encoder_input_word_to_index = None
+    decoder_input_word_to_index = None
+    index_to_word_eng = None
+    index_to_word_trans = None
+
+    max_encoder_input_seq_length = 0
+    max_decoder_input_seq_length = 0
+    encoder_input_vocab_size = 0
+    decoder_input_vocab_size = 0
+
+    padded_encoder_inputs = []
+    padded_decoder_inputs = []
+    padded_decoder_outouts = []
+
+    encoder_word_embedding_layer: Embedding = None
+    decoder_one_hot_target = None
+
+    encoder_model: Model = None
+    decoder_model: Model = None
+
+    @staticmethod
+    def initialize():
+        WSeq2Seq.generate_text_array()
+        WSeq2Seq.generate_encoder_input_tokenizer()
+        WSeq2Seq.generate_decoder_input_tokenizer()
+        WSeq2Seq.generate_encoder_input_word_to_index()
+        WSeq2Seq.generate_decoder_input_word_to_index()
+        WSeq2Seq.transform_texts_to_seqs()
+        WSeq2Seq.generate_input_vocab_sizes()
+        WSeq2Seq.generate_input_max_seq_lengths()
+        WSeq2Seq.generate_padded_seqs()
+        WSeq2Seq.generate_encoder_word_embedding_layer()
+        WSeq2Seq.generate_decoder_one_hot_target()
+        WSeq2Seq.generate_index_to_words()
+
+    @staticmethod
+    def generate_text_array():
+        t = 0
+        for line in open("./jpn.txt", encoding="utf8"):
+            t = t + 1
+            if t > WSeq2Seq.NUM_SAMPLES:
+                break
+
+            if "\t" not in line:
+                continue
+
+            values = line.split("\t")
+            input_text = values[0]
+            translation = values[1]
+
+            target_text = translation + " <eos>"
+            target_text_input = "<sos> " + translation
+
+            WSeq2Seq.encoder_texts_input.append(input_text)
+            WSeq2Seq.decoder_texts_output.append(target_text)
+            WSeq2Seq.decoder_texts_input.append(target_text_input)
+
+    @staticmethod
+    def generate_encoder_input_tokenizer():
+        WSeq2Seq.encoder_input_tokenizer = Tokenizer(num_words=WSeq2Seq.MAX_VOCAB_SIZE, filters="")
+        WSeq2Seq.encoder_input_tokenizer.fit_on_texts(WSeq2Seq.encoder_texts_input)
+
+    @staticmethod
+    def generate_decoder_input_tokenizer():
+        WSeq2Seq.decoder_input_tokenizer = Tokenizer(num_words=WSeq2Seq.MAX_VOCAB_SIZE, filters="")
+        # just to make sure both <sos> and <tos> are included, not efficient
+        WSeq2Seq.decoder_input_tokenizer.fit_on_texts(WSeq2Seq.decoder_texts_output + WSeq2Seq.decoder_texts_input)
+
+    @staticmethod
+    def generate_encoder_input_word_to_index():
+        WSeq2Seq.encoder_input_word_to_index = WSeq2Seq.encoder_input_tokenizer.word_index
+
+    @staticmethod
+    def generate_decoder_input_word_to_index():
+        WSeq2Seq.decoder_input_word_to_index = WSeq2Seq.decoder_input_tokenizer.word_index
+
+    @staticmethod
+    def transform_texts_to_seqs():
+        WSeq2Seq.encoder_input_seq = WSeq2Seq.encoder_input_tokenizer.texts_to_sequences(WSeq2Seq.encoder_texts_input)
+        WSeq2Seq.decoder_output_seq = WSeq2Seq.decoder_input_tokenizer.texts_to_sequences(WSeq2Seq.decoder_texts_output)
+        WSeq2Seq.decoder_input_seq = WSeq2Seq.decoder_input_tokenizer.texts_to_sequences(WSeq2Seq.decoder_texts_input)
+
+    @staticmethod
+    def generate_input_vocab_sizes():
+        # +1 because we also include padding index 0 which correspond to nth in our vocab list
+        # this becomes essential in creating embedding matrix
+        # note that keras use word_count + 1 as the index of UNKNOWN, the key is "None" by default (already generated in word_index)
+        WSeq2Seq.encoder_input_vocab_size = len(WSeq2Seq.encoder_input_tokenizer.word_index) + 1
+        WSeq2Seq.decoder_input_vocab_size = len(WSeq2Seq.decoder_input_tokenizer.word_index) + 1
+
+    @staticmethod
+    def generate_input_max_seq_lengths():
+        WSeq2Seq.max_encoder_input_seq_length = max(len(s) for s in WSeq2Seq.encoder_input_seq)
+        WSeq2Seq.max_decoder_input_seq_length = max(len(s) for s in WSeq2Seq.decoder_input_seq)
+
+    @staticmethod
+    def generate_padded_seqs():
+        WSeq2Seq.padded_encoder_inputs = pad_sequences(
+            WSeq2Seq.encoder_input_seq,
+            maxlen=WSeq2Seq.max_encoder_input_seq_length
+        )
+        # for LSTM training part input and output are of same length in sentences size
+        WSeq2Seq.padded_decoder_inputs = pad_sequences(
+            WSeq2Seq.decoder_input_seq,
+            maxlen=WSeq2Seq.max_decoder_input_seq_length,
+            padding="post"
+        )
+        WSeq2Seq.padded_decoder_outouts = pad_sequences(
+            WSeq2Seq.decoder_output_seq,
+            maxlen=WSeq2Seq.max_decoder_input_seq_length,
+            padding="post"
+        )
+
+    @staticmethod
+    def generate_encoder_word_embedding_layer():
+        print("Loading word vectors...")
+        word_to_vec = {}
+        with open(os.path.join("./glove.6B.{}d.txt".format(WSeq2Seq.EMBEDDING_DIM)), encoding="utf8") as f:
+            for line in f:
+                values = line.split()
+                word = values[0]
+                vec = np.array(values[1:], dtype="float32")
+                word_to_vec[word] = vec
+
+        print("filling pre-trained embeddings")
+        num_words = min(
+            WSeq2Seq.MAX_VOCAB_SIZE,
+            len(WSeq2Seq.encoder_input_word_to_index) + 1
+        )
+        embedding_matrix = np.zeros((num_words, WSeq2Seq.EMBEDDING_DIM))
+
+        # for embedding matrix, we are just interested in words in our training set:
+        for word, index in WSeq2Seq.encoder_input_word_to_index.items():
+            word_vec = word_to_vec.get(word)
+            if word_vec is not None:
+                embedding_matrix[index] = word_vec
+
+        WSeq2Seq.encoder_word_embedding_layer = Embedding(
+            num_words,
+            WSeq2Seq.EMBEDDING_DIM,
+            weights=[embedding_matrix],
+            input_length=WSeq2Seq.max_encoder_input_seq_length
+        )
+
+    @staticmethod
+    def generate_decoder_one_hot_target():
+        decoder_one_hot_targets = np.zeros(
+            (
+                len(WSeq2Seq.decoder_texts_output),
+                WSeq2Seq.max_decoder_input_seq_length,
+                WSeq2Seq.decoder_input_vocab_size
+            ),
+            dtype="float32"
+        )
+        # e.g., decoder_targets_one_hot[0] is an array of binarized vectors, [e_1, e_4, ..., e_10], where e_k denotes the standard basis in R^num_words_output
+        for i, wordDigits in enumerate(WSeq2Seq.decoder_output_seq):
+            for t, digit in enumerate(wordDigits):
+                decoder_one_hot_targets[i, t, digit] = 1
+
+        WSeq2Seq.decoder_one_hot_target = decoder_one_hot_targets
+
+    @staticmethod
+    def generate_index_to_words():
+        WSeq2Seq.index_to_word_eng = {v: k for k, v in WSeq2Seq.encoder_input_word_to_index.items()}
+        WSeq2Seq.index_to_word_trans = {v: k for k, v in WSeq2Seq.decoder_input_word_to_index.items()}
+
+    @staticmethod
+    def build_encoder_decoder_model():
+        # build the model
+        # input is expected to be an array of integers
+        encoder_inputs_placeholder = Input(shape=(WSeq2Seq.max_encoder_input_seq_length,))
+        x = WSeq2Seq.encoder_word_embedding_layer(encoder_inputs_placeholder)
+        encoder = LSTM(WSeq2Seq.LATENT_DIM, return_state=True, dropout=0.5)
+        _, h, c = encoder(x)
+        encoder_states = [h, c]
+
+        decoder_inputs_placeholder = Input(shape=(WSeq2Seq.max_decoder_input_seq_length,))
+        # it is a different langauge, we use different embedding
+        # as we don't have pretrained embedding layer for japanese words, we just apply built-in one:
+        decoder_embedding = Embedding(WSeq2Seq.decoder_input_vocab_size, WSeq2Seq.LATENT_DIM)
+        decoder_inputs_x = decoder_embedding(decoder_inputs_placeholder)
+        decoder_lstm = LSTM(WSeq2Seq.LATENT_DIM, return_sequences=True, return_state=True, dropout=0.5)
+        decoder_outputs, _, _ = decoder_lstm(
+            decoder_inputs_x,
+            initial_state=encoder_states
+        )
+
+        decoder_dense = Dense(WSeq2Seq.decoder_input_vocab_size, activation="softmax")
+        decoder_outputs = decoder_dense(decoder_outputs)
+
+        model = Model([encoder_inputs_placeholder, decoder_inputs_placeholder], decoder_outputs)
+
+        model.compile(
+            optimizer="rmsprop",
+            loss="categorical_crossentropy",
+            metrics=["acc"]
+        )
+
+        r = model.fit(
+            [WSeq2Seq.padded_encoder_inputs, WSeq2Seq.padded_decoder_inputs], WSeq2Seq.decoder_one_hot_target,
+            batch_size=WSeq2Seq.BATCH_SIZE,
+            epochs=WSeq2Seq.EPOCHS,
+            validation_split=0.2
+        )
+
+        plt.plot(r.history["loss"], label="loss")
+        plt.plot(r.history["val_loss"], label="val_loss")
+        plt.legend()
+        plt.show()
+
+        plt.plot(r.history["acc"], label="acc")
+        plt.plot(r.history["val_acc"], label="val_acc")
+        plt.legend()
+        plt.show()
+
+        encoder_model = Model(encoder_inputs_placeholder, encoder_states)
+
+        decoder_state_input_h = Input(shape=(WSeq2Seq.LATENT_DIM,))
+        decoder_state_input_c = Input(shape=(WSeq2Seq.LATENT_DIM,))
+        decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+
+        decoder_inputs_single = Input(shape=(1,))
+        decoder_inputs_single_x = decoder_embedding(decoder_inputs_single)
+
+        decoder_outputs, h, c = decoder_lstm(
+            decoder_inputs_single_x,
+            initial_state=decoder_states_inputs
+        )
+
+        decoder_states = [h, c]
+        decoder_outputs = decoder_dense(decoder_outputs)
+
+        decoder_model = Model(
+            [decoder_inputs_single] + decoder_states_inputs,
+            [decoder_outputs] + decoder_states
+        )
+
+        WSeq2Seq.encoder_model = encoder_model
+        WSeq2Seq.decoder_model = decoder_model
+
+    @staticmethod
+    def decode_sequence(input_seq):
+        states_value = WSeq2Seq.encoder_model.predict(input_seq)
+        target_seq = np.zeros((1, 1))
+        target_seq[0, 0] = WSeq2Seq.decoder_input_word_to_index["<sos>"]
+        eos_index = WSeq2Seq.decoder_input_word_to_index["<eos>"]
+
+        output_sentence = []
+        for _ in range(WSeq2Seq.max_decoder_input_seq_length):
+            output_tokens, h, c = WSeq2Seq.decoder_model.predict(
+                [target_seq] + states_value
+            )
+
+            idx = np.argmax(output_tokens[0, 0, :])
+
+            if idx == eos_index:
+                break
+
+            word = ""
+            if idx > 0:
+                word = WSeq2Seq.index_to_word_trans[idx]
+                output_sentence.append(word)
+
+            target_seq[0, 0] = idx
+            states_value = [h, c]
+
+        return " ".join(output_sentence)
+
+    @staticmethod
+    def random_translation():
+        while True:
+            i = np.random.choice(len(WSeq2Seq.encoder_texts_input))
+            input_seq = WSeq2Seq.padded_encoder_inputs[i:i+1]
+            translation = WSeq2Seq.decode_sequence(input_seq)
+            print("-")
+            print("Input: ", WSeq2Seq.encoder_texts_input[i])
+            print("Translation: ", translation)
+
+            ans = input("Continue? [Y/n]")
+
+            if ans and ans.lower().startswith("n"):
+                break
 
 
-t = 0
-for line in open("./jpn.txt", encoding="utf8"):
-    t = t + 1
-    if t > NUM_SAMPLES:
-        break
-
-    if "\t" not in line:
-        continue
-
-    values = line.split("\t")
-    input_text = values[0]
-    translation = values[1]
-
-    target_text = translation + " <eos>"
-    target_text_input = "<sos> " + translation
-
-    input_texts.append(input_text)
-    target_texts.append(target_text)
-    target_texts_inputs.append(target_text_input)
-
-
-tokenizer_inputs = Tokenizer(num_words=MAX_NUM_WORDS, filters="")
-tokenizer_inputs.fit_on_texts(input_texts)
-input_seqs = tokenizer_inputs.texts_to_sequences(input_texts)
-
-word2idx_inputs = tokenizer_inputs.word_index
-max_len_input = max(len(s) for s in input_seqs)
-
-
-tokenizer_outputs = Tokenizer(num_words=MAX_NUM_WORDS, filters="")
-# make sure to save both <sos> and <eos>
-# this is to save both <sos> and <eos>, very inefficient though
-tokenizer_outputs.fit_on_texts(target_texts + target_texts_inputs)
-
-target_seqs = tokenizer_outputs.texts_to_sequences(target_texts)
-target_inputs_seqs = tokenizer_outputs.texts_to_sequences(target_texts_inputs)
-
-
-word2idx_outputs = tokenizer_outputs.word_index
-
-# keras use word_count+1 as the index of UNKNOWN, the key is "None" by default.
-# note that keras also starts the index at 1
-# 0 is the padding value for input
-num_words_output = len(word2idx_outputs) + 1
-
-# max length of outputs seq
-max_len_target = max(len(s) for s in target_seqs)
-
-# pad the seqs, input_seqs come from training data
-# by default padding="pre", i.e., add zeros at the beginning
-encoder_inputs = pad_sequences(
-    input_seqs,
-    maxlen=max_len_input,
-)
-
-print("encoder_data.shape", encoder_inputs.shape)
-print("encoder_inputs[0]", encoder_inputs[0])
-
-decoder_inputs = pad_sequences(
-    target_inputs_seqs,
-    maxlen=max_len_target,
-    padding="post"
-)
-
-print("decoder_inputs.shape", decoder_inputs.shape)
-print("decoder_inputs[0]", decoder_inputs[0])
-
-# for LSTM training part input and output are of same length in sentences size
-decoder_targets = pad_sequences(
-    target_seqs,
-    maxlen=max_len_target,
-    padding="post"
-)
-
-print("Loading word vectors...")
-word2vec = {}
-
-with open(os.path.join("./glove.6B.{}d.txt".format(EMBEDDING_DIM))) as f:
-    for line in f:
-        values = line.split()
-        word = values[0]
-        vec = np.array(values[1:], dtype="flaot32")
-        word2vec[word] = vec
-
-print("filling pre-trained embeddings")
-
-# include 0:
-num_words = min(MAX_NUM_WORDS, len(word2idx_inputs) + 1)
-embedding_matrix = np.zeros((num_words, EMBEDDING_DIM))
-
-# for embedding matrix, we are just interested in words in our training set:
-# word2idx_inputs comes from tokenizer of our training inputs
-for word, index in word2idx_inputs.items():
-    word_vec = word2vec.get(word)
-    if word_vec is not None:
-        embedding_matrix[index] = word_vec
-
-embedding_layer = Embedding(
-    num_words,
-    EMBEDDING_DIM,
-    weights=[embedding_matrix],
-    input_length=max_len_input
-)
-
-decoder_targets_one_hot = np.zeros(
-    (len(input_texts), max_len_target, num_words_output),
-    dtype="float32"
-)
-
-# e.g., decoder_targets_one_hot[0] is an array of binarized vectors, [e_1, e_4, ..., e_10], where e_k denotes the standard basis in R^num_words_output
-for i, wordDigits in enumerate(decoder_targets):
-    for t, digit in enumerate(wordDigits):
-        decoder_targets_one_hot[i, t, digit] = 1
-
-# build the model
-# input is expected to be an array of integers
-encoder_inputs_placeholder = Input(shape=(max_len_input,))
-x = embedding_layer(encoder_inputs_placeholder)
-encoder = LSTM(LATENT_DIM, return_state=True, dropout=0.5)
-encoder_outputs, h, c = encoder(x)
-
-
-encoder_states = [h, c]
-
-
-decoder_inputs_placeholder = Input(shape=(max_len_target,))
-
-# it is a different langauge, we use different embedding
-decoder_embedding = Embedding(num_words_output, LATENT_DIM)
-decoder_inputs_x = decoder_embedding(decoder_inputs_placeholder)
-decoder_lstm = LSTM(LATENT_DIM, return_sequences=True, return_state=True, dropout=0.5)
-decoder_outputs, _, _ = decoder_lstm(
-    decoder_inputs_x,
-    initial_state=encoder_states
-)
-
-decoder_dense = Dense(num_words_output, activation="softmax")
-decoder_outputs = decoder_dense(decoder_outputs)
-
-model = Model([encoder_inputs_placeholder, decoder_inputs_placeholder], decoder_outputs)
-
-model.compile(
-    optimizer="rmsprop",
-    loss="categorical_crossentropy",
-    metrics=["acc"]
-)
-r = model.fit(
-    [encoder_inputs, decoder_inputs], decoder_targets_one_hot,
-    batch_size=BATCH_SIZE,
-    epochs=EPOCHS,
-    validation_split=0.2
-)
-
-
-plt.plot(r.history["loss"], label="loss")
-plt.plot(r.history["val_loss"], label="val_loss")
-plt.legend()
-plt.show()
-
-plt.plot(r.history["acc"], label="acc")
-plt.plot(r.history["val_acc"], label="val_acc")
-plt.legend()
-plt.show()
-
-# num_words = 3
-# tk = Tokenizer(num_words=num_words+1, oov_token="UNK")
-# texts = ["my name is far faraway asdasd", "my name is","your name is"]
-# tk.fit_on_texts(texts)
-# # see #8092 below why I do these two line
-# tk.word_index = {e:i for e,i in tk.word_index.items() if i <= num_words} # <= because tokenizer is 1 indexed
-# tk.word_index[tk.oov_token] = num_words + 1
-
-# # output
-# print(tk.word_index)
-# wihtout oov_token, UNK is replaced by "None"
-# {'name': 1, 'my': 3, 'is': 2, 'UNK': 4}
-# print(tk.texts_to_sequences(texts))
-# [[3, 1, 2, 4, 4, 4], [3, 1, 2], [4, 1, 2]]
+WSeq2Seq.initialize()
+WSeq2Seq.build_encoder_decoder_model()
+WSeq2Seq.random_translation()
